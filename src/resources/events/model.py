da3ce356855748d30f749faf4bbd347773353b52
bentoluizv <bentoluizv@gmail.com>
2025-05-18 19:06:20 -0300

    Add Event model for SQLAlchemy
    - Introduced the Event model in `src/resources/events/model.py` to represent event data in the database.
    - Defined attributes such as id, edition, title, description, start_date, end_date, location, image_url, is_active, is_published, created_at, and updated_at with appropriate data types and constraints.
    - Utilized SQLAlchemy's mapped_column for ORM functionality and included default values where necessary.

---
+++ src/resources/events/model.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, Integer, String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.resources import Base
+from src.utils import generate_ulid
+
+
+class Event(Base):
+    """Event model for SQLAlchemy."""
+
+    __tablename__ = 'events'
+
+    id: Mapped[str] = mapped_column(
+        String(26),
+        primary_key=True,
+        default=generate_ulid,
+    )
+
+    edition: Mapped[int] = mapped_column(Integer, nullable=False, index=True, unique=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    start_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    location: Mapped[str] = mapped_column(String(255), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    is_published: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
