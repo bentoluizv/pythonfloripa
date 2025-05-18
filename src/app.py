b540b69abd67b8a29929138ad0cdc95dbded0277
bentoluizv <bentoluizv@gmail.com>
2025-05-18 16:15:32 -0300

    Integrate user management router into FastAPI application
    - Added the user management router to the main FastAPI app, enabling access to user-related API endpoints.
    - Updated the application structure to include the new router for improved organization and modularity.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from src.resources.users.router import router as users_router
+
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+app.include_router(users_router)
