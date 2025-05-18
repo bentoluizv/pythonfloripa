e5b243db2550689f25cc9015788e52cbc9a8a692
bentoluizv <bentoluizv@gmail.com>
2025-05-18 19:07:41 -0300

    Import events model in resources initialization
    - Added an import statement for the events model in `src/resources/__init__.py` to facilitate access to event-related functionalities within the resources module.
    - This change enhances the modular structure of the application by integrating event management capabilities.

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -5,6 +5,7 @@ class Base(DeclarativeBase):
     pass
 
 
+from src.resources.events import model as events_model  # noqa: E402, F401
 from src.resources.users import model as users_model  # noqa: E402, F401
 
 __all__ = ['Base']
