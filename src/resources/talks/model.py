5cff381a7a9f6c144e23d98fb8df30d5e5868381
bentoluizv <bentoluizv@gmail.com>
2025-05-18 20:53:31 -0300

    Add Talk model for event management
    - Introduced a new Talk model in `src/resources/talks/model.py` to represent talks associated with events.
    - The model includes fields for id, title, description, speaker, start and end times, and a foreign key relationship with the Event model.
    - This addition enhances the application's capability to manage and organize talks within events, supporting better data structure and relationships.

---
+++ src/resources/talks/model.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import DateTime, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.resources import Base
+from src.utils import generate_ulid
+
+
+class Talk(Base):
+    __tablename__ = 'talks'
+
+    id: Mapped[str] = mapped_column(
+        String(26),
+        primary_key=True,
+        default=generate_ulid,
+    )
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    speaker: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+
+    event_id: Mapped[str] = mapped_column(
+        String(26),
+        ForeignKey('events.id', ondelete='RESTRICT'),
+        nullable=False,
+    )
+    event: Mapped['Event'] = relationship(back_populates='talks', lazy='joined')  # type: ignore # noqa: F821
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=func.now(), onupdate=func.now()
+    )
