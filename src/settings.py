a390358720d76951fd2ea0197ef870ded8f6154c
bentoluizv <bentoluizv@gmail.com>
2025-05-19 09:58:16 -0300

    Enhance settings configuration for database connection
    - Expanded the Settings class to include individual PostgreSQL connection parameters: POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, and POSTGRES_DB.
    - Added a new method, database_url, to generate the database URL with an option to hide the password, improving security and usability for database connections.

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,41 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy.engine.url import URL
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    """
+    Settings for the application.
+    """
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+    def database_url(self, hide_password: bool = False) -> str:
+        """
+        Returns the database URL.
+        If hide_password is True, the password will be hidden.
+        If hide_password is False, the password will be shown.
+
+        Example:
+        >>> settings = Settings()
+        >>> settings.database_url(hide_password=True)
+        'postgresql+asyncpg://user:********@host:5432/db'
+        >>> settings.database_url(hide_password=False)
+        'postgresql+asyncpg://user:password@host:5432/db'
+        """
+        return URL.create(
+            drivername='postgresql+asyncpg',
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            database=self.POSTGRES_DB,
+        ).render_as_string(hide_password=hide_password)
 
 
 def get_settings() -> Settings:
