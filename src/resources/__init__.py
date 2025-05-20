10c5c12938c359a80b888f2fba1a79d5aef1adf2
bentoluizv <bentoluizv@gmail.com>
2025-05-20 10:41:06 -0300

    Update __init__.py to include speakers_model in exports
    - Added speakers_model to the __all__ export list in the __init__.py file, allowing for easier access to the speakers resource in other modules.
    - This change enhances the modularity and organization of the resource imports.

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -6,7 +6,8 @@ class Base(DeclarativeBase):
 
 
 from src.resources.events import model as events_model  # noqa: E402, F401
+from src.resources.speakers import model as speakers_model  # noqa: E402, F401
 from src.resources.talks import model as talks_model  # noqa: E402, F401
 from src.resources.users import model as users_model  # noqa: E402, F401
 
-__all__ = ['Base']
+__all__ = ['Base', 'events_model', 'speakers_model', 'talks_model', 'users_model']
