22f8fca5a76842f74dda95bf0b80c3ce8a75c8ae
bentoluizv <bentoluizv@gmail.com>
2025-05-20 10:41:18 -0300

    Integrate speakers router into main application
    - Added the speakers router to the FastAPI application, enabling the management of speaker-related endpoints.
    - This integration enhances the application's modularity and allows for streamlined access to speaker functionalities.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.resources.events.router import router as events_router
+from src.resources.speakers.router import router as speakers_router
 from src.resources.talks.router import router as talks_router
 from src.resources.users.router import router as users_router
 
@@ -15,3 +16,4 @@ async def root():
 app.include_router(users_router)
 app.include_router(events_router)
 app.include_router(talks_router)
+app.include_router(speakers_router)
